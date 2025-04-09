af33427222908f0811a0e7d8ef93cbbdd2fae89b
Ahmed Yousef <yousef.noor1616@gmail.com>
2025-04-09 13:56:41 +0800

    Create Content  component in  part1/courseinfo/src/App.jsx

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,28 +6,31 @@ const Header = (props) => {
   )
 }
 
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const partExercises = [
+    { part: 'Fundamentals of React', exercises: 10},
+    { part: 'Using props to pass data', exercises: 7},
+    { part: 'State of a component', exercises: 14}
+
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part={partExercises[0].part} exercises={partExercises[0].exercises} />
+      <Content part={partExercises[1].part} exercises={partExercises[1].exercises} />
+      <Content part={partExercises[2].part} exercises={partExercises[2].exercises} />
     </div>
   )
 }
