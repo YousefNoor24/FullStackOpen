cf2268157bc7299fde084533a09cea8be0850966
Ahmed Yousef <yousef.noor1616@gmail.com>
2025-05-17 22:04:52 +0800

    Edit part2/courseinfo/src/App.jsx part2/courseinfo/src/components/Course.jsx

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,33 +1,33 @@
 const Header = (props) => <h1>{props.course}</h1>
 
 const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
+    <div>
+        {props.parts.map(part => <Part key={part.id} part={part} />)}
+    </div>
 )
 
 const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
+    <p>
+        {props.part.name} {props.part.exercises}
+    </p>
 )
+const Total = (props) => {
+    const total = props.parts.reduce((totalValue, currentValue) => totalValue + currentValue.exercises, 0)
+    return (
+        <p><b>total of {total} exercises</b></p>
+    )
+}
 
-const Total = (props) => <p>Number of exercises {props.total}</p>
-
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
     return (
         <>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total
-                total={
-                course.parts[0].exercises +
-                course.parts[1].exercises +
-                course.parts[2].exercises
-                }
-            />
+            {courses.map(course => 
+                <div key={course.id}>
+                    <Header course={course.name} />
+                    <Content parts={course.parts} />
+                    <Total parts={course.parts}/>
+                </div>
+            )}
         </>
     )
 }
