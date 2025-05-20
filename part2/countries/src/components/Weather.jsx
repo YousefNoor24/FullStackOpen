1dcc4c11668ada090a9d4926c605cc1b670b2111
Ahmed Yousef <yousef.noor1616@gmail.com>
2025-05-20 21:42:34 +0800

    Edit part2/countries/src/components/Weather.jsx

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -3,21 +3,23 @@ import weatherService from "../services/weather"
 
 const Weather = ({name, latitude, longitude}) => {
     const [weather, setWeather] = useState(null)
-
+    console.log(name, latitude, longitude)
     useEffect(() => {
         weatherService
             .getWeather(latitude, longitude)
-            .then(weather => setWeather(weather))
+            .then(weather => {
+                setWeather(weather)
+            })
     }, [])
 
     if (weather === null) return null
-
+    console.log(weather.list[0].weather[0].icon)
     return (
         <div>
             <h2>Weather in {name}</h2>
-            <p>Temperature {weather.main.temp} Celsius</p>
-            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt={weather.weather[0].description}/>
-            <p>Wind {weather.wind.speed} m/s</p>
+            <p>Temperature {weather.list[0].main.temp} Celsius</p>
+            <img src={`https://openweathermap.org/img/wn/${weather.list[0].weather[0].icon}.png`} alt={weather.list[0].weather[0].description}/>
+            <p>Wind {weather.list[0].wind.speed} m/s</p>
         </div>
     )
 }
