252b2fcf3a25dae8b431d26fb69a27c997acd28d
Ahmed Yousef <yousef.noor1616@gmail.com>
2025-05-20 21:43:04 +0800

    Edit part2/countries/src/services/weather.js

--- part2/countries/src/services/weather.js
+++ part2/countries/src/services/weather.js
@@ -1,11 +1,14 @@
 import axios from "axios"
 
-const api_key = import.meta.env.VITE_OPENWEATHER_KEY
-const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
+const api_key = import.meta.env.VITE_SOME_KEY
+const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast'
+
 
 const getWeather = (latitude, longitude) => {
     const request = axios.get(`${baseUrl}?lat=${latitude}&lon=${longitude}&appid=${api_key}`)
+    console.log(request)
     return request.then(request => request.data)
 }
+
 
 export default {getWeather}
