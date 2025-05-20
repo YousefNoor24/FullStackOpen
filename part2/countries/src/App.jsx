19d81a8cac3383449d0d1fc9195e3a173b9542c6
Ahmed Yousef <yousef.noor1616@gmail.com>
2025-05-20 18:48:30 +0800

    Edit part2/countries/src/App.jsx

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Content from './components/Content'
 import Filter from './components/Filter'
 import countriesService from './services/countries'
 
@@ -14,65 +15,6 @@ const App = () => {
       })
   }, [])
 
-  const Countries = ({ countries, selectCountry }) => {
-    return (
-      <div>
-        {countries.map(country => (
-          <div key={country.name.common}>
-            <span>{country.name.common}</span>
-            <button onClick={() => selectCountry(country)}>Show</button>
-          </div>
-        ))}
-      </div>
-    )  
-  }
-
-  const Country = ({country}) => {
-    const keyLanguages = Object.keys(country.languages)
-
-    return (
-      <div>
-        <h2>{country.name.common}</h2>
-        <div>Capital {country.capital}</div>
-        <div>Area {country.area}</div>
-        <h2>Languages</h2>
-        <ul>
-          {keyLanguages.map(key => (
-            <li key={key}>{country.languages[key]}</li>
-          ))}
-        </ul>
-        <img src={country.flags.png} />
-      </div>
-    )
-  }
-
-  const Content = ({array, selectCountry}) => {
-    if (array.length > 10) {
-      return (
-        <div>Too many matches,specify another filter</div>
-      )
-    }
-
-    if (array.length > 1 && array.length <= 10) {
-      return (
-        <Countries countries={array} selectCountry={selectCountry}/>
-      )
-    }
-
-    if (array.length === 1) {
-      return (
-        <Country country={array[0]} />
-      )
-    }
-
-    return (
-      <div>
-        No Matches Found
-      </div>
-    )
-
-  }
-
   const handleChangeFilter = (event) => {
     setNewFilter(event.target.value)
   }
@@ -84,6 +26,7 @@ const App = () => {
   }
 
   const showCountries = newFilter.trim().length === 0 ? countries : filteredCountries();
+
   const selectCountry = (country) => {
     console.log(country.name.common)
     setNewFilter(country.name.common)
