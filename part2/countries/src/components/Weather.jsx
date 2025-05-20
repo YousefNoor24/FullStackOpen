9b037813392999b334911f552c1c4c7c15d767c1
Ahmed Yousef <yousef.noor1616@gmail.com>
2025-05-20 21:46:59 +0800

    Edit part2/countries/src/components/Weather.jsx

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -3,7 +3,7 @@ import weatherService from "../services/weather"
 
 const Weather = ({name, latitude, longitude}) => {
     const [weather, setWeather] = useState(null)
-    console.log(name, latitude, longitude)
+    
     useEffect(() => {
         weatherService
             .getWeather(latitude, longitude)
@@ -13,7 +13,7 @@ const Weather = ({name, latitude, longitude}) => {
     }, [])
 
     if (weather === null) return null
-    console.log(weather.list[0].weather[0].icon)
+
     return (
         <div>
             <h2>Weather in {name}</h2>
